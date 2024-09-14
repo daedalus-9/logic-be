2c8261571bb5a5f8e1fc530ae17fd36174cbbb89
Adam <adamyoungy678@gmail.com>
2024-09-14 20:48:54 +0100

    fix: Comment out unused routes currently and add express import

--- routes.js
+++ routes.js
@@ -1,92 +1,95 @@
 // routes.js
 const { body, validationResult } = require("express-validator");
 const { sendEmailReceipt } = require("./email");
-const {
-  setDoc,
-  getDocs,
-  where,
-  collection,
-  query,
-} = require("firebase/firestore");
-const db = require("./firebaseConfig");
+const express = require("express");
+// const {
+//   setDoc,
+//   getDocs,
+//   where,
+//   collection,
+//   query,
+// } = require("firebase/firestore");
+// const db = require("./firebaseConfig");
 require("dotenv").config();
+
+const appExpress = express();
 
 const defineRoutes = (app) => {
   // Promotion route
-  app.post(
-    "/promotion",
-    [
-      body("fullname")
-        .trim()
-        .notEmpty()
-        .withMessage("Full name is required")
-        .escape(),
-      body("email")
-        .trim()
-        .isEmail()
-        .withMessage("Invalid email address")
-        .escape(),
-      body("phone")
-        .trim()
-        .notEmpty()
-        .withMessage("Phone number is required")
-        .escape(),
-      body("receipts")
-        .optional()
-        .isBoolean()
-        .withMessage("Receipts must be a boolean value"),
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
+//   app.post(
+//     "/promotion",
+//     [
+//       body("fullname")
+//         .trim()
+//         .notEmpty()
+//         .withMessage("Full name is required")
+//         .escape(),
+//       body("email")
+//         .trim()
+//         .isEmail()
+//         .withMessage("Invalid email address")
+//         .escape(),
+//       body("phone")
+//         .trim()
+//         .notEmpty()
+//         .withMessage("Phone number is required")
+//         .escape(),
+//       body("receipts")
+//         .optional()
+//         .isBoolean()
+//         .withMessage("Receipts must be a boolean value"),
+//     ],
+//     async (req, res) => {
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//       }
 
-      const { fullname, email, phone, receipts } = req.body;
+//       const { fullname, email, phone, receipts } = req.body;
 
-      try {
-        const colRef = collection(db, "promotion");
+//       try {
+//         const colRef = collection(db, "promotion");
 
-        const emailQuery = query(colRef, where("email", "==", email));
-        const emailSnapshot = await getDocs(emailQuery);
+//         const emailQuery = query(colRef, where("email", "==", email));
+//         const emailSnapshot = await getDocs(emailQuery);
 
-        if (!emailSnapshot.empty) {
-          return res.status(400).json({ message: "Email is already in use." });
-        }
+//         if (!emailSnapshot.empty) {
+//           return res.status(400).json({ message: "Email is already in use." });
+//         }
 
-        const phoneQuery = query(colRef, where("phone", "==", phone));
-        const phoneSnapshot = await getDocs(phoneQuery);
+//         const phoneQuery = query(colRef, where("phone", "==", phone));
+//         const phoneSnapshot = await getDocs(phoneQuery);
 
-        if (!phoneSnapshot.empty) {
-          return res
-            .status(400)
-            .json({ message: "Phone number is already in use." });
-        }
+//         if (!phoneSnapshot.empty) {
+//           return res
+//             .status(400)
+//             .json({ message: "Phone number is already in use." });
+//         }
 
-        const docRef = doc(colRef);
-        await setDoc(docRef, { fullname, email, phone, createdAt: new Date() });
+//         const docRef = doc(colRef);
+//         await setDoc(docRef, { fullname, email, phone, createdAt: new Date() });
 
-        if (receipts) {
-          await sendEmailReceipt(email, fullname, phone);
-        }
+//         if (receipts) {
+//           await sendEmailReceipt(email, fullname, phone);
+//         }
 
-        res.status(200).json({ message: "Promotion data saved successfully." });
-      } catch (error) {
-        console.error("Error saving promotion data:", error);
-        res.status(500).json({ message: "Error saving data to Firestore." });
-      }
-    }
-  );
+//         res.status(200).json({ message: "Promotion data saved successfully." });
+//       } catch (error) {
+//         console.error("Error saving promotion data:", error);
+//         res.status(500).json({ message: "Error saving data to Firestore." });
+//       }
+//     }
+//   );
 
   // Cron job route to keep server alive
-  app.get("/cron-job-route", (req, res) => {
-    const serverUrl = process.env.SERVER_URL;
-    retryFetch(serverUrl)
-      .then(() => res.sendStatus(200))
-      .catch((error) =>
-        res.status(500).json({ message: "Error pinging server", error })
-      );
-  });
+//   app.get("/cron-job-route", (req, res) => {
+//     const serverUrl = process.env.SERVER_URL;
+//     retryFetch(serverUrl)
+//       .then(() => res.sendStatus(200))
+//       .catch((error) =>
+//         res.status(500).json({ message: "Error pinging server", error })
+//       );
+//   });
 
   appExpress.post(
     "/",
